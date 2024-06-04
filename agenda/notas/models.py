808dad78d32043d05c39318fd0cc2de783fd9716
nuevo_63 <ajrahuacso@gmail.com>
2024-06-03 22:35:30 -0500

    Se crean los modelos

---
+++ agenda/notas/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Notas(models.Model):  
+    fecha = models.DateField()
+    titulo = models.CharField(max_length = 100)
+    contenido = models.TextField()
+
+    def __str__(self):
+        return f"La nota fue guardada en: {self.fecha} con el título '{self.titulo}' y el contenido '{self.contenido[:50]}'"
