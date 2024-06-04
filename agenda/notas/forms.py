483c1989114479c518e0899eadae7ef111061456
RyanValdivia <rvaldiviase@unsa.edu.pe>
2024-06-03 23:40:17 -0500

    Ya funcionan los forms y eso

--- agenda/notas/forms.py
+++ agenda/notas/forms.py
@@ -1,14 +1,18 @@
+from datetime import date
 from django import forms
 from .models import *
 
 class NotaForm(forms.ModelForm):
-    fecha = models.DateField()
+    fecha = models.DateField(default=date.today)
     titulo = models.CharField(max_length = 100)
     contenido = models.TextField()
 
     class Meta:
         model = Nota
         fields = ['fecha', 'titulo', 'contenido']
+        widgets = {
+            'fecha': forms.DateInput(attrs={'type': 'date'}),
+        }
 
     def save(self, commit=True):
         nota = super().save(commit=False)
